99ec3e17cd56cba6b18da342b3681f4a1f8b0c2e
Vinicius Felipe <75151794+vinifeeh@users.noreply.github.com>
2024-07-03 19:15:11 -0300

    Add files via upload

---
+++ contactthanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Mensagem Enviada!</title>
+	
+</head>
+
+<body>
+		
+	<h1>Sua mensagem foi enviada com sucesso!</h1>
+
+</body>
+</html>
